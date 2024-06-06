7b13c34f3e52227a615136078517a2806d2c80d0
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-06 11:12:39 +0200

    added function and varibles

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 document.getElementById("aver").onclick = function avg(){
+    let Math = Number(document.getElementById("input1").value)
+    let Science =Number(document.getElementById("input2").value)
+    let Trade = Number(document.getElementById("input3").value)
+    let Industrial = Number(document.getElementById("input4").value)
+    let Fundementals = Number(document.getElementById("input5").value)
+    let Practicles = Number(document.getElementById("input6").value)
+    let averageM = (((Math + Science + Trade + Industrial + Fundementals + Practicles) / 100) / 6)
