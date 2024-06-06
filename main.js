2ade9f3295401edee7fdb7a34175d08a31598ed3
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-06 11:22:31 +0200

    added function if and else statement

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ document.getElementById("aver").onclick = function avg(){
     let Fundementals = Number(document.getElementById("input5").value)
     let Practicles = Number(document.getElementById("input6").value)
     let averageM = (((Math + Science + Trade + Industrial + Fundementals + Practicles) / 100) / 6)
+
+
+    if (averageM >= 65 ){
+        alert(`Your Average is   ${averageM}  You have Passed !`)
+    }
+
+
+    else {
+        alert(`Your Average is   ${averageM}  You have Failed !`)
+
+    }
+
+
+}
